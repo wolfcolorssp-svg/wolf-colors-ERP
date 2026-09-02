a9fe5ee73005578c045023f258ddb79517cc4955
wolfcolorssp-svg <wolfcolors.sp@gmail.com>
2026-09-02 01:01:42 -0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+
 import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import Clientes from './pages/Clientes';
